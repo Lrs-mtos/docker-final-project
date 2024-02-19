f4f8664f1cbe39cb1b2c31b73a527b460c7e8d15
Larissa Matos <larissamatos07.lm@gmail.com>
2024-02-19 16:05:10 -0300

    changes on psql

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,12 @@
-CREATE TABLE teste (
+CREATE TABLE IF NOT EXISTS exemplo (
     id SERIAL PRIMARY KEY,
-    nome TEXT NOT NULL
+    nome TEXT NOT NULL,
+    descricao TEXT,
+    data_criacao DATE NOT NULL DEFAULT CURRENT_DATE
 );
+
+INSERT INTO exemplo (nome, descricao) VALUES 
+('Amaranth', 'A purple grain cultivated by an ancient civilization'),
+('Kale', 'The waxy leaves are great in soups and stir frys'),
+('Cave Carrot', 'A starchy snack found in caves. It helps miners work longer'),
+('Hops', 'A bitter, tangy flower used to flavor beer');
