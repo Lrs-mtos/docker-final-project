fdf6c6828b159e8b5da5abba364c394fd4a54b14
Larissa Matos <larissamatos07.lm@gmail.com>
2024-02-19 19:49:19 -0300

    docker-compose update and aplication update

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 import psycopg2
 import os
 
@@ -11,8 +11,24 @@ def get_db_connection():
                             password=os.getenv('DATABASE_PASSWORD'))
     return conn
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        # Processa os dados enviados pelo formulário
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+        # Assume-se que data_criacao é automaticamente definida pelo banco de dados
+        
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO exemplo (nome, descricao) VALUES (%s, %s)", (nome, descricao))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect('/')
+    
+    # Se não for um POST, mostra os registros existentes e o formulário
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute('SELECT * FROM exemplo;')
@@ -20,9 +36,18 @@ def index():
     cur.close()
     conn.close()
     
-    # Construindo uma lista HTML para visualização no navegador
     items = '<br>'.join([f"{row[0]}, {row[1]}, {row[2]}, {row[3]}" for row in exemplo_data])
-    return f"<html><body>{items}</body></html>"
+    form = '''
+        <form method="post">
+            Nome:<br>
+            <input type="text" name="nome"><br>
+            Descrição:<br>
+            <input type="text" name="descricao"><br>
+            <input type="submit" value="Adicionar Item">
+        </form>
+    '''
+    return f"<html><body>{form}{items}</body></html>"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
